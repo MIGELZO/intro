4c9d66c15b79d3ccd0f7e19183039974ba8b9c6a
MIGELZO <154762117+MIGELZO@users.noreply.github.com>
2024-03-02 23:42:26 +0200

    final modify 23.42

--- src/tasks/components/TaskComponent.jsx
+++ src/tasks/components/TaskComponent.jsx
@@ -1,32 +1,22 @@
 import { TableBody, TableCell, TableRow } from '@mui/material'
 import React from 'react'
 
-export default function TaskComponent({name,date,type}) {
+export default function TaskComponent({task}) {
   
   return (
     <TableBody>
       <TableRow>
         <TableCell>
-          {name[0]}
+          {task[0]}
         </TableCell>
         <TableCell>
-          {type[0]}
+          {task[1]}
         </TableCell>
         <TableCell>
-          {date[0]}
+          {task[2]}
         </TableCell>
       </TableRow>
-      <TableRow>
-        <TableCell>
-          {name[1]}
-        </TableCell>
-        <TableCell>
-          {type[1]}
-        </TableCell>
-        <TableCell>
-          {date[1]}
-        </TableCell>
-      </TableRow>
+
     </TableBody>
   )
 }
--- src/tasks/components/TasksPageComponent.jsx
+++ src/tasks/components/TasksPageComponent.jsx
@@ -4,15 +4,19 @@ import { Paper, Table, TableContainer } from "@mui/material";
 import TaskComponent from "./TaskComponent";
 
 export default function TaskPageComponent({tasks}) {
-    let taskName = tasks.map((tasks=>{return tasks.name}));
-    let taskType = tasks.map((tasks=>{return tasks.type}));
-    let taskDate = tasks.map((tasks=>{return tasks.date}));
-    
+  let taskName = tasks.map((tasks=>{return tasks.name}));
+  let taskType = tasks.map((tasks=>{return tasks.type}));
+  let taskDate = tasks.map((tasks=>{return tasks.date}));
+
+  let task1 = [taskName[0], taskType[0], taskDate[0]]
+  let task2 = [taskName[1], taskType[1], taskDate[1]]
+  
   return (
         <TableContainer component={Paper} sx={{width:700, m: 2}}>
       <Table sx={{ minWidth: 400}} aria-label="simple table">
         <TaskHeadComponent/>
-        <TaskComponent name={taskName} date={taskDate} type={taskType}/>
+        <TaskComponent task={task1}/>
+        <TaskComponent task={task2}/>
       </Table>
     </TableContainer>
   );
